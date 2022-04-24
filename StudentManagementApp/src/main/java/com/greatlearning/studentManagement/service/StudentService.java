e7ee4e53f882b1e8651a97140e16b5afea7ef7bd
SiddharthNaharGH <siddharthnahar@hotmail.com>
2022-04-25 00:16:51 +0530

    Lab-SpringAssignmentSolution

---
+++ StudentManagementApp/src/main/java/com/greatlearning/studentManagement/service/StudentService.java
@@ -0,0 +1,20 @@
+package com.greatlearning.studentManagement.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.greatlearning.studentManagement.entity.Student;
+
+
+public interface StudentService {
+	
+	public List<Student> findAll();
+
+	public Student findById(int theId);
+
+	public void save(Student thestudent);
+
+	public void deleteById(int theId);
+
+}
